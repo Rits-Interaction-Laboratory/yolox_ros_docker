4b1399bc39709cbc380f7a7a38ea8138cb61e900
aarimoto <67216471+arinnko5@users.noreply.github.com>
2023-02-24 13:15:50 +0900

    Update my_exp_yolox_x.py

--- settings/YOLOX-ROS/my_exp_yolox_x.py
+++ settings/YOLOX-ROS/my_exp_yolox_x.py
@@ -1,1 +1,25 @@
+import os
+from yolox.exp import Exp as MyExp
+
+class Exp(MyExp):
+    def __init__(self):
+        super(Exp, self).__init__()
+        self.depth = 1.33
+        self.width = 1.25
+        self.exp_name = os.path.split(os.path.realpath(__file__))[1].split(".")[0]
+
+        self.data_dir = "datasets/dataset"
+        self.train_ann = "instances_train2017.json"
+        self.val_ann = "instances_val2017.json"
+
+        self.num_classes = 80
+        self.max_epoch = 150
+        self.eval_interval = 10
+
+
+    def get_model(self):
+        from yolox.utils import freeze_module
+        model = super().get_model()
+        freeze_module(model.backbone.backbone)
+        return model
 
